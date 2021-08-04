99dc5e15e48890d4d85093f4b53570d7f0921c2c
SimuNews <tobias.pieber@htl-braunau.at>
2021-08-04 21:29:34 +0200

    VRC-30: Created entry point for the device with arguments (or read later from file) Added additional LogCenter and Helper for future use

---
+++ TreadRun.Core/TreadRun.Core/Helpers/LogCenter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreadRun.Core.Helpers
+{
+    enum LogLevel
+    {
+        Info,
+        Warning,
+        Error,
+        Fatal,
+        FatalWarning,
+        FatalError
+    }
+
+    public class LogCenter
+    {
+        public static LogCenter Instance { get; private set; } = null;
+
+        public LogCenter()
+        {
+            Instance = Instance ?? this;
+        }
+
+        #region static methods
+
+        public static void Initialize()
+        {
+            Instance = new LogCenter();
+        }
+
+        #endregion
+
+        #region private methods
+
+        private void Log(LogLevel logLevel, string message)
+        {
+            Console.WriteLine(string.Format("[{0} at {1}]: {2}", logLevel, DateTime.Now, message));
+        }
+
+        #endregion
+
+        #region public methods
+
+        public void LogInfo(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Log(LogLevel.Warning, message);
+        }
+
+        public void LogError(string message)
+        {
+            Log(LogLevel.Error, message);
+        }
+
+        public void LogError(Exception exception)
+        {
+            Log(LogLevel.Error, exception.StackTrace);
+        }
+
+        public void LogFatal(string message)
+        {
+            Log(LogLevel.Fatal, message);
+        }
+        public void LogFatalWarning(string message)
+        {
+            Log(LogLevel.FatalWarning, message);
+        }
+        public void LogFatalError(string message)
+        {
+            Log(LogLevel.FatalError, message);
+        }
+        public void LogFatalError(Exception exception)
+        {
+            Log(LogLevel.FatalError, exception.StackTrace);
+        }
+
+        #endregion
+
+    }
+}
