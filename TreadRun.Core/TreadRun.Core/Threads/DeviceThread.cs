2cff3ff836df629bafa3d7e3401b760ff7342201
Tobias Pieber <t.pieber@sofa1.at>
2021-08-05 12:01:19 +0200

    VRC-33: Added RaspberryIO to the Project and initialized it

---
+++ TreadRun.Core/TreadRun.Core/Threads/DeviceThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
+
+namespace TreadRun.Core.Threads
+{
+    class DeviceThread
+    {
+        public static async Task StartAsync()
+        {
+            //Init RaspberryIO
+            Pi.Init<BootstrapWiringPi>();
+
+            while (true)
+            {
+                
+
+                await Task.Delay(1);
+            }
+        }
+    }
+}
