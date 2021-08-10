10d4f9b6f20e15cd48b080d70fcb7c36751923b4
Tobias Pieber <t.pieber@sofa1.at>
2021-08-10 10:07:20 +0200

    Added CalibrationService.cs

---
+++ TreadRun.Core/TreadRun.Core/Service/CalibrationService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreadRun.Core.Calibration;
+
+namespace TreadRun.Core.Services
+{
+    class CalibrationService
+    {
+		#region threadsafe singleton
+
+		private static volatile CalibrationService _instance;
+		private static readonly object SyncRoot = new object();
+
+		public static CalibrationService Instance
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				if (_instance == null)
+				{
+					lock (SyncRoot)
+					{
+						if (_instance == null)
+							_instance = new CalibrationService();
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+        #endregion
+
+        #region public properties
+
+        // Register calibration models
+        public VelocityCalibration VelocityCalibration { get; set; }
+
+        #endregion
+
+        #region public methods
+
+        public void Initialize()
+		{
+			CreateNewServiceInstances();
+		}
+
+		public void CreateNewServiceInstances()
+		{
+			VelocityCalibration = new VelocityCalibration();
+		}
+
+        #endregion
+    }
+}
